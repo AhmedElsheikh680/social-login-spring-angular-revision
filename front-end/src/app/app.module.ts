8ecfa657fc8cbb6163fb1a2f987862b07e06cc6c
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2021-07-31 14:04:05 +0200

    HomeCoponent ProfileComponent SocialComponent

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,13 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { SocialComponent } from './components/social/social.component';
+import {RouterModule, Routes} from "@angular/router";
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'social', component: SocialComponent },
+  { path: '**', redirectTo: '/home' },
+  { path: '', component: HomeComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    ProfileComponent,
+    SocialComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule
   ],
   providers: [],
